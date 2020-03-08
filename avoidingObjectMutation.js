a62529b281317899e64b2103c51d595886889222
Justine Francis <jfvalmores@gmail.com>
2020-03-08 23:35:01 +0800

    done tutorial

---
+++ avoidingObjectMutation.js
@@ -0,0 +1,28 @@
+const toggleTodo = (todo) => {
+  return {
+    ...todo,
+    completed: true
+  };
+}
+
+const testToggleTodo = () => {
+  const todoBefore = {
+    id: 0,
+    text: 'Learn Redux',
+    completed: false
+  };
+
+  const todoAfter = {
+    id: 0,
+    text: 'Lear Redux',
+    completed: true
+  };
+
+  deepFreeze(todoBefore);
+
+  expect(
+    toggleTodo(todoBefore)
+  ).toEqual(todoAfter);
+};
+
+testToggleTodo();
---
+++ counter.js
@@ -0,0 +1,72 @@
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+const { createStore } = Redux;
+// const createStore = (reducer) => {
+//   let state;
+//   let listener;
+
+//   const getState = () => state;
+
+//   const dispatch = (action) => {
+//     state = reducer(state, action);
+//     listener.forEach(listener => listener());
+//   };
+
+//   const subscribe = (listener) => {
+//     listener.push(listener);
+//     return () => {
+//       listeners = listeners.filter(l => l !== listener)
+//     }
+//   };
+
+//   return {
+//     getState,
+//     dispatch,
+//     subscribe,
+//   };
+// }
+
+const Counter = ({
+  value,
+  onIncrement,
+  onDecrement
+}) => (
+    <div>
+      <h1>{value}</h1>
+      <button onClick={onIncrement}>+</button>
+      <button onClick={onDecrement}>-</button>
+    </div>
+  );
+
+const store = createStore(counter);
+
+const render = () => {
+  ReactDOM.render(
+    <Counter
+      value={store.getState()}
+      onIncrement={() =>
+        store.dispatch({
+          type: 'INCREMENT'
+        })
+      }
+      onDecrement={() =>
+        store.dispatch({
+          type: 'DECREMENT'
+        })
+      }
+    />,
+    document.getElementById('root')
+  );
+}
+
+store.subscribe(render);
+render();
